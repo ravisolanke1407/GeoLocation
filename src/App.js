134d1e1bf09e3a2872aaa6c32a0142f395535e9a
unknown <ravindra>
2021-09-14 20:10:12 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import React from "react"
+import UseGeoLocation from "./CustomHook/UseGeoLocation"
+
+
+function Map(){
+  const geoLocation = UseGeoLocation();
+  console.log("location",JSON.stringify(geoLocation))
+  return <GoogleMap 
+  defaultZoom={8}
+  defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    <Marker position={{ lat: -34.397, lng: 150.644 }} />
+  </GoogleMap>
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div style={{ height: '100vh', width: '100%' }}>
+        
+      <WrappedMap
+        // isMarkerShown
+        googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDeTXs8t-ZU6FBOCzheyZ8puzj8LoCaoDg"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
+      </div>
   );
 }
 
